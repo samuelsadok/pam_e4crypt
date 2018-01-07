54d1a5b75938d1786201919fc7519a3c6626a83f
Samuel Sadok <samuel.sadok@bluewin.ch>
2018-01-07 15:10:19 +0100

    make shell script safer

--- init_wrapper_files.sh
+++ init_wrapper_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -eou pipefail
 
 SALTPATH="/home/.e4crypt/salt"
 WRAPPATH="/home/.e4crypt/wrap"
@@ -23,24 +23,24 @@ then
     done
 fi
 
-sudo mkdir -p $SALTPATH
-sudo rm -f $SALTPATH/$USER
-sudo rm -rf $WRAPPATH/$USER
-sudo touch $SALTPATH/$USER
-sudo mkdir -p $WRAPPATH/$USER
-head -c $EXT4_MAX_SALT_SIZE /dev/urandom | sudo tee $SALTPATH/$USER > /dev/null
+sudo mkdir -p "$SALTPATH"
+sudo rm -f "$SALTPATH/$USER"
+sudo rm -rf "$WRAPPATH/$USER"
+sudo touch "$SALTPATH/$USER"
+sudo mkdir -p "$WRAPPATH/$USER"
+head -c $EXT4_MAX_SALT_SIZE /dev/urandom | sudo tee "$SALTPATH/$USER" > /dev/null
 echo -n "Enter passphrase (echo disabled): "
-e4crypt_out=$(e4crypt add_key -S 0x$(sudo xxd -c 9999 -p $SALTPATH/$USER))
+e4crypt_out="$(e4crypt add_key -S 0x$(sudo xxd -c 9999 -p "$SALTPATH/$USER"))"
 echo ""
 echo "$e4crypt_out" | tail +2
 outerkeyhash=$(echo "$e4crypt_out" | grep -oP '(?<=\[)[0-9a-f]+(?=\])')
-sudo e4crypt set_policy $outerkeyhash $WRAPPATH/$USER
-head -c $EXT4_MAX_SALT_SIZE /dev/urandom | sudo tee $WRAPPATH/$USER/salt > /dev/null
-echo -n `head -c $((EXT4_MAX_PASSPHRASE_SIZE / 2)) /dev/urandom | xxd -c 99999 -p` | sudo tee $WRAPPATH/$USER/auth > /dev/null
-sudo chmod -R 700 $SALTPATH
-sudo chmod -R 700 $WRAPPATH
-sudo chgrp $USER $SALTPATH/$USER
-sudo chgrp $USER $WRAPPATH/$USER
+sudo e4crypt set_policy $outerkeyhash "$WRAPPATH/$USER"
+head -c $EXT4_MAX_SALT_SIZE /dev/urandom | sudo tee "$WRAPPATH/$USER/salt" > /dev/null
+echo -n `head -c $((EXT4_MAX_PASSPHRASE_SIZE / 2)) /dev/urandom | xxd -c 99999 -p` | sudo tee "$WRAPPATH/$USER/auth" > /dev/null
+sudo chmod -R 700 "$SALTPATH"
+sudo chmod -R 700 "$WRAPPATH"
+sudo chgrp $USER "$SALTPATH/$USER"
+sudo chgrp $USER "$WRAPPATH/$USER"
 
 yn=
 while [ "$yn" != "n" -a "$yn" != "Y" ]
@@ -49,6 +49,6 @@ do
     read yn
     if [ "$yn" == "Y" ]
     then
-        sudo cat $WRAPPATH/$USER/auth | e4crypt add_key -S 0x$(sudo xxd -c 9999 -p $WRAPPATH/$USER/salt) | tail +2
+        sudo cat "$WRAPPATH/$USER/auth" | e4crypt add_key -S 0x$(sudo xxd -c 9999 -p "$WRAPPATH/$USER/salt") | tail +2
     fi
 done
